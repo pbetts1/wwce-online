a742116d94946ef321bfc2fc12091fe2d081ec0e
kenny <kkinds@gmail.com>
2013-06-30 17:12:56 -0500

    updates

--- application/views/advanced_view.php
+++ application/views/advanced_view.php
@@ -21,7 +21,7 @@
 					<?php 
 									echo form_fieldset('Basic Search');
 							        echo form_label('Find an Intervention');	
-									echo form_input('basic');
+									echo form_input('basic', set_value('basic'));
 					                echo form_fieldset_close(); 
 					                  ?>
 		
@@ -36,7 +36,7 @@
 									echo form_fieldset('Overall Designation');
 							        echo form_label('Intervention Effectiveness');	 
 										 $options = array(
-						                  'selected'  => 'Select one...',
+						                  ''  => 'Select one...',
 						                  'Effective'    => 'Effective',
 						                  'Unproven'   => 'Unproven',
 						                  'Iatrogenic' => 'Iatrogenic'); 
@@ -53,7 +53,7 @@
 							echo form_fieldset('Target Population');
 							echo form_label('Grade Level Intervention');
                              $options = array(
-						                  'selected'  => 'Select one...',
+						                  ''  => 'Select one...',
 						                  'Elementary'    => 'Elementary',
 						                  'Middle School'   => 'Middle School',
 						                  'High School' => 'High School'); 
@@ -77,7 +77,7 @@
 							echo form_fieldset('Outcome Category');
 							echo form_label('Outcome of Intervention');
                              $options = array(
-						                  'selected'  => 'Select one...',
+						                  ''  => 'Select one...',
 						                  'Risk Behavior'    => 'Risk Behavior',
 						                  'Competencies'   => 'Pro-Social Competencies',
 						                  'School Based outcome' => 'School-Based outcome',
@@ -94,7 +94,7 @@
 							echo form_fieldset('Implementation Strategy');
 							echo form_label('Content and Pedagogical Elements');
                              $options = array(
-						                  'selected'  => 'Select one...',
+						                  ''  => 'Select one...',
 						                  'Explicit Chatacter Education Program'    => 'Explicit Chatacter Education Program',
 						                  'Social and Emotional Curriculum'   => 'Social and Emotional Curriculum',
 						                  'Academic Curriculum Integration' => 'Academic Curriculum Integration',
