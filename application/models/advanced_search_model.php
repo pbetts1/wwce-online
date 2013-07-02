177e96d108c60f0278e31e3b7102349298e069cd
kenny <kkinds@gmail.com>
2013-07-02 11:55:41 -0500

    adding comments

--- application/models/advanced_search_model.php
+++ application/models/advanced_search_model.php
@@ -1,9 +1,11 @@
 <?php
 class Advanced_search_model extends CI_Model {
 
+//The model for the advanced search page
+
 	public function get_search()
 	{
-			
+			//Get the form varibles and put them in an associative array
 				$query_array = array(
 				
 				'option'=> $this->input->post('option'),
@@ -13,31 +15,33 @@ class Advanced_search_model extends CI_Model {
 				'strategy' => $this->input->post('strategy'),
 				'outcomes' => $this->input->post('outcomes')
 				);
-				
-			
-				
-				
-				
+		
+		//Check if this is an and or search
 				if (strlen($query_array['option']) AND $query_array['option']=="or"){
 					
-					
+		//start query			
 			    $this->db->select('*');
 				$this->db->from('synopsis_header');
-				
+
+               //if this the Intervention Effectiveness field is selected add it to the query				
 				if (strlen($query_array['effective'])){
 				$this->db->like('Overall_designation',$query_array['effective'],'both');
 				}
-				
+			
+			   //if this the Grade Level Intervention field is selected add it to the query		
 				if (strlen($query_array['target'])){
 				$this->db->or_like('Target_population',$query_array['target'],'both');
 				}
 				
+			 //if this the Basic field is has text add it to the query		
 				if (strlen($query_array['basic'])){
+				$match = $query_array['basic'];
 				$this->db->or_like('Delivery_Agents',$match, 'both');
 				$this->db->or_like('Initial_Mission',$match, 'both');
                 $this->db->or_like('Program_Synopsis',$match, 'both');
 				}
 				
+				//"and" search	
 				} elseif (strlen($query_array['option']) AND $query_array['option']=="and") {
 					
 					
@@ -53,6 +57,7 @@ class Advanced_search_model extends CI_Model {
 				}
 				
 				if (strlen($query_array['basic'])){
+				$match = $query_array['basic'];	
 				$this->db->like('Delivery_Agents',$match, 'both');
 				$this->db->like('Initial_Mission',$match, 'both');
                 $this->db->like('Program_Synopsis',$match, 'both');
@@ -61,9 +66,10 @@ class Advanced_search_model extends CI_Model {
 					
 				}
 				
-	
+	            //get query results
 				$query = $this->db->get();
 				
+				//return query results
 				return $query->result();   
 				
 	   
